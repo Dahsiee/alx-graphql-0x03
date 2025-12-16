b62af098ced43170452b7c40941a401ad6a654fc
Dahisee <chuksgabriella2019@gmail.com>
2025-12-16 15:13:25 +0000

    ...

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApolloProvider} from "@apollo/client/react";
 import client from "@/graphql/apolloClient";
+import ErrorBoundary from '@/components/ErrorBoundary';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,3 +11,13 @@ export default function App({ Component, pageProps }: AppProps) {
     </ApolloProvider>
   )
 }
+
+function MyApp({Component, pageProps}:AppProps) {
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  )
+}
+
+export default MyApp;
